5b1fd71799c6ca6474d5d37f9260b16ea82efff3
Aji Kamaludin <aji19kamaludin@gmail.com>
2021-08-01 13:17:59 +0700

    consumer program finished

---
+++ src/MailSender.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer');
+
+class MailSender {
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      // secure: true,
+      auth: {
+        user: process.env.MAIL_ADDRESS,
+        pass: process.env.MAIL_PASSWORD,
+      },
+    });
+
+    this.sendEmail = this.sendEmail.bind(this);
+  }
+
+  sendEmail(targetEmail, content) {
+    const message = {
+      from: 'Notes Apps',
+      to: targetEmail,
+      subject: 'Ekspor Catatan',
+      text: 'Terlampir hasil dari ekspor catatan',
+      attachments: [
+        {
+          filename: 'notes.json',
+          content,
+        },
+      ],
+    };
+
+    return this._transporter.sendMail(message);
+  }
+}
+
+module.exports = MailSender;
